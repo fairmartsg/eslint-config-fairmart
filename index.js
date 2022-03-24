3a277b4fb81e0b81c5adb944b9bb67c93b2952a1
Daniil Moskovtsov <qa000100@gmail.com>
2022-03-24 20:10:18 +0800

    2.0.8

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = {
         "max-len": [
             "error",
             {
-                "code": 100
+                "code": 120
             }
         ],
         "indent": "off",
