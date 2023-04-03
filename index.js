eb4861aaa41592321a039d73a9f6e696cb5264ca
lusivelin <linhtetpaing.dev@gmail.com>
2023-04-03 16:22:07 +0630

    fix: update for extends typescript-eslint

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ module.exports = {
     'extends': [
         'eslint:recommended',
         "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
         'plugin:react/all',
         'plugin:sonarjs/recommended'
     ],
