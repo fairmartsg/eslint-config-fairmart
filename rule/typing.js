64c9d6493bdac95a57a49c2cd95fdde26294e9fa
daniil <daniil@fairmart.app>
2023-08-02 17:56:31 +0800

    3.3.2 Adding types checks

--- rule/typing.js
+++ rule/typing.js
@@ -5,6 +5,9 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking"
     ],
+    "parserOptions": {
+        "project": ["./tsconfig.json"]
+    },
     "rules": {
         "@typescript-eslint/explicit-function-return-type": "warn",
         "@typescript-eslint/explicit-module-boundary-types": "warn",
